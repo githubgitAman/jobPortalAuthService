fbb06759e8b0c55337d424fff4287ab1bf96d334
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-17 19:01:03 +0530

    Created Job Portal Authentication Service

---
+++ src/test/java/dev/aman/jobportalauthservice/JobPortalAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
